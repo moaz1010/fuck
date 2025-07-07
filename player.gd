89c94297ce5b7c31a374b559f1ec4b59daf35726
Omar Anwar <anwrmr649@gmail.com>
2025-07-07 23:12:03 +0300

    added more input compatability
    added joystick support and keyboard arrows
    *and changed "move_keft" to "move_left"

--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ const SPEED = 200
 
 func _physics_process(_delta):
 	
-	var direction = Input.get_vector("move_keft", "move_right", "move_up", "move_down")
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
 	velocity = direction.normalized() * SPEED
 	move_and_slide()
