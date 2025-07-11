57361e1c45c931dfad4824df675569ab6d065d08
Omar Anwar <anwrmr649@gmail.com>
2025-07-12 01:15:54 +0300

    did some shit in the movement

--- custom_nodes/moving_entity.gd
+++ custom_nodes/moving_entity.gd
@@ -1,55 +1,64 @@
 extends CharacterBody2D
 class_name MovingEntity
 
+
 @export var SECONDS_TO_MAX_SPEED := .9 
 @export var SECONDS_TO_STOP_COMPLETELY := .4
+@export var HANG_TIME := .5
 
 @export var MAX_SPEED := 500.0
-@export var JUMP_VELOCITY := 400.0
+@export var FALL_GRAVITY_MULTIPLIER := 2.2
 
-@export var MAX_SPEED_JUMP_INCREASE := .1
 
 var speed_jump_multiplier : float = 1
 var direction : float = 0
 var ACCELERATION : float
 var FRICTION : float
+var GRAVITY : float = 200
+#TODO: CHANGE THESE TO BE RELATIVE TO CHARACTER HEIGHT
+@export var MAX_SPEED_JUMP_INCREASE := .1
+@export var JUMP_VELOCITY := 400.0
+
 
 func _init() -> void:
-	
+
 	ACCELERATION = MAX_SPEED / SECONDS_TO_MAX_SPEED
 	FRICTION = MAX_SPEED / SECONDS_TO_STOP_COMPLETELY
-	
+
+	GRAVITY = JUMP_VELOCITY / HANG_TIME
 	#Adjusting acceleration, since friction is applied even when moving.
 	ACCELERATION += FRICTION
 
 
 func _physics_process(delta: float) -> void:
-	
-	
+
+
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
-		
+	if velocity.y >= 0:
+		velocity.y += GRAVITY * FALL_GRAVITY_MULTIPLIER * delta
+	else:
+		velocity.y += GRAVITY * delta
+
+
 	#Handle acceleration.
 	if direction:
 		velocity.x += direction * ACCELERATION * delta
-		
-		
+
+
 	#Handle friction.
 	var friciton_direction : int = -(sign(velocity.x))
 	velocity.x += FRICTION * friciton_direction * delta
 	
 	if friciton_direction == sign(velocity.x):
 		velocity.x = 0
-		
-		
+
+
 	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED) #Clamping speed.
-	
-	
+
+
 	speed_jump_multiplier = ((abs(velocity.x) / MAX_SPEED) * MAX_SPEED_JUMP_INCREASE) + 1
-	
-		
+
+
 	move_and_slide()
 
 func jump():
