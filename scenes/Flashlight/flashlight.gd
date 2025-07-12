e4573af788151355286f603c8054265221fb7367
Omar Anwar <anwrmr649@gmail.com>
2025-07-12 17:17:18 +0300

    made basic flashlight

--- scenes/Flashlight/flashlight.gd
+++ scenes/Flashlight/flashlight.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+@onready var light : Area2D = $Area2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,3 +12,11 @@ func _ready() -> void:
 
 func _process(_delta):
 	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("fire"):
+		light.visible = !light.visible
+		light.monitoring = !light.monitoring
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
