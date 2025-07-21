97158ccc1cab4b4557d5b214759b5ba13fc783eb
GremTaK <zoztail573@gmail.com>
2025-07-21 21:36:01 +0300

    added settings menu (not finished)

--- scenes/pop_up_menu.gd
+++ scenes/pop_up_menu.gd
@@ -2,12 +2,35 @@ extends MarginContainer
 
 @export var menu_screen: VBoxContainer
 @export var open_menu_screen: VBoxContainer
+@export var open_menu_button: Button
+@export var close_menu_button: Button
+
+
+func _process(delta: float) -> void:
+	update_button_scale()
+
+func update_button_scale():
+	button_hov(open_menu_button, 1.5, 0.2)
+	button_hov(close_menu_button, 1.5, 0.2)
+
+func button_hov(button: Button, tween_amt, duration):
+	button.pivot_offset = button.size / 2
+	if button.is_hovered():
+		tween(button, "scale", Vector2.ONE * tween_amt, duration)
+	else:
+		tween(button, "scale", Vector2.ONE, duration)
+
+func tween(button, property, amount, duration):
+	var tween = create_tween()
+	tween.tween_property(button, property, amount, duration)
 
 func toggle_visibility(object):
 	if object.visible:
 		object.visible = false 
 	else:
 		object.visible = true
+		
+
 
 
 
@@ -15,3 +38,4 @@ func toggle_visibility(object):
 func _on_toggle_menu_button_pressed() -> void:
 	toggle_visibility(menu_screen)
 	toggle_visibility(open_menu_screen)
+	
