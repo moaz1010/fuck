c3cdad912280a3256c7e153e46d334e747decb9b
GremTaK <zoztail573@gmail.com>
2025-07-23 00:01:13 +0300

    added dialog system (not finished not even halfway)

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -6,8 +6,10 @@ extends MovingEntity   #yo can we like add comments next shit we add so that i c
 
 var was_on_floor: bool = true
 var wants_to_jump: bool = false
+var can_move: bool = true
 
 func _ready() -> void:
+	add_to_group("player")
 	super()
 	Inventory.added_weapon.connect(
 		func(resource: InvWeaponResource): 
@@ -24,6 +26,8 @@ func _input(_event: InputEvent) -> void:
 
 
 func _process(_delta: float) -> void:
+	if !can_move:
+		return
 	if not is_on_floor() and was_on_floor:
 		coyote_timer.start()
 	was_on_floor = is_on_floor()
