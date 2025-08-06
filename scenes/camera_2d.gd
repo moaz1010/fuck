7dbf8ce0af5cd39b17f130118f7a38ba831c85eb
Omar Anwar <anwrmr649@gmail.com>
2025-08-07 01:48:55 +0300

    fix camera glitch

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -1,12 +1,15 @@
-extends Camera2D
+extends Node2D
 
 var desired_offset: Vector2
-var min_offset = -200
-var max_offset = 200 
+var max_offset : int = 50 
+var resistance : int = 5
 
-func _process(delta: float) -> void:
-	desired_offset = (get_global_mouse_position() * position) * 0.5
-	desired_offset.x = clamp(desired_offset.x , min_offset, max_offset)
-	desired_offset.y = clamp(desired_offset.y , min_offset/2.0 , max_offset/2.0)
-	
-	global_position = get_parent().get_node("player").global_position + desired_offset
+func _process(_delta: float) -> void:
+	desired_offset = (
+		get_viewport().get_mouse_position() - 
+		get_viewport().get_visible_rect().size/2
+		) / resistance
+	desired_offset.x = clamp(desired_offset.x , -max_offset, max_offset)
+	desired_offset.y = clamp(desired_offset.y , -max_offset , max_offset)
+	#desired_offset *= abs(desired_offset.normalized())
+	position = desired_offset
