de711ca0023c041d211ee8bc64ba5ae756a7ec77
Omar Anwar <anwrmr649@gmail.com>
2025-08-12 23:36:43 +0300

    make weapon change cursor

---
+++ components/cursor_component.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name CursorComponent
+
+enum Hotspots {
+	TOP_LEFT,
+	CENTER
+}
+
+@export var image : Texture2D
+@export var hotspot : Hotspots
+@export var shape : Input.CursorShape
+
+var hotspot_position : Vector2 = Vector2.ZERO
+
+
+func _ready() -> void:
+	if hotspot == Hotspots.CENTER:
+		hotspot_position = image.get_size()/2
+	Input.set_custom_mouse_cursor(image, shape, hotspot_position)
