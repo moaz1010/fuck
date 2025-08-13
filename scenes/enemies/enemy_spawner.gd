6e6ccb90f5e48a382d5ef6fe957b459d4051b2c4
GremTaK <zoztail573@gmail.com>
2025-08-13 17:34:09 +0300

    added enemy spawn area ( not working )

---
+++ scenes/enemies/enemy_spawner.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var enemy_prefab: PackedScene  # to get which enemy to spawn
+@export var num_of_enemies: int       # to choose how many enemies spawn
+
+func _ready():
+	Flags.activated_flag.connect(_on_flag_activated)
+
+func _on_flag_activated(flag):
+	if flag == flag.ENTERED_ENEMY_SPAWN_AREA:
+		$Timer.start()
+	
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		Flags.activate_flag(Flags.flags_enum.ENTERED_ENEMY_SPAWN_AREA)
+		
+		
+		
+
+func _on_timer_timeout() -> void:
+	var enemy = enemy_prefab.instantiate()
+		
