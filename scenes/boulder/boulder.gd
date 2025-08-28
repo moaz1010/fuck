3d8558c08800a684deb3105aae48f4fcb17c0beb
Omar Anwar <anwrmr649@gmail.com>
2025-08-28 18:24:42 +0300

    fix boulder

--- scenes/boulder/boulder.gd
+++ scenes/boulder/boulder.gd
@@ -1,10 +1,12 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var gravity := 500.0
+var should_start_falling : bool = false
 
-
-func _process(delta: float) -> void:
-	if $AnimationPlayer.animation_finished:
-		position.y += 1
+func _physics_process(delta: float) -> void:
+	if should_start_falling:
+		velocity.y += gravity*delta
+	move_and_slide()
 
 func _on_enter_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -13,4 +15,4 @@ func _on_enter_area_body_entered(body: Node2D) -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "shaking":
-		pass
+		should_start_falling = true
