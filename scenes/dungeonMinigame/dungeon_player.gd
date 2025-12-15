d40f919b7620621a6fcb8d676014d7e98e39c706
GremTaK <egyptian10102010@gmail.com>
2025-12-15 17:14:49 +0200

    made a simple change to dungeon player movement

--- scenes/dungeonMinigame/dungeon_player.gd
+++ scenes/dungeonMinigame/dungeon_player.gd
@@ -21,4 +21,4 @@ func _move(dir: Vector2):
 		sprite_node_position_tween.kill()
 	sprite_node_position_tween = create_tween()
 	sprite_node_position_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	sprite_node_position_tween.tween_property($Sprite2D, "global_position", global_position, 0.185).set_trans(Tween.TRANS_SINE)
+	sprite_node_position_tween.tween_property($Sprite2D, "global_position", global_position, 0.005).set_trans(Tween.TRANS_SINE)
